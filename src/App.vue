c263f2db5b267e2e87aaca6a01e7dbe9f4e4c211
Akai <2900426287@qq.com>
2023-08-19 15:24:15 +0800

    build: 增加代码格式化插件

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,15 @@
 <template>
-<div>
-  123
-</div>
+  <div class="box">1234556</div>
 </template>
 
-<script setup lang='ts'>
+<script setup lang="ts">
 // import { ref , reactive } from 'vue'
-
+const aa = 'hhhh'
+console.log(aa)
 </script>
-<style scoped>
 
+<style scoped lang="ts">
+.box{
+  width: 100px;
+}
 </style>
